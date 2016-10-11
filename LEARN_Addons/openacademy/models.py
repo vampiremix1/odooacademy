ac1323ad9078fbb8df35fe7035ab299da3b1d4c6
vampiremix1 <vampiremix1@gmail.com>
2016-10-11 15:57:37 +0700

    Edit model.py

--- LEARN_Addons/openacademy/models.py
+++ LEARN_Addons/openacademy/models.py
@@ -2,6 +2,9 @@
 
 from openerp import models, fields, api
 
+class Course(models.model):
+	_name = 'openacademy.course'
+	name = fields.Char()
 # class openacademy(models.Model):
 #     _name = 'openacademy.openacademy'
 
